753897b3aa4140f4efc1a35956e8e6ec1fc3bbfd
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-06-05 12:40:00 +0200

    Feat: add PWA & optimization :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app.routing';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
   ],
   imports: [
     CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}
