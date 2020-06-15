13df7937252d97b6401f1e56cb5c11bdb7acc53d
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-06-15 15:42:09 +0200

    Feat: add in training status :sparkles:

--- src/app/core/enums/intent-status.enum.ts
+++ src/app/core/enums/intent-status.enum.ts
@@ -2,6 +2,7 @@ export enum IntentStatus {
   to_deploy = 'to_deploy',
   active = 'active',
   active_modified = 'active_modified',
+  in_training = 'in_training',
   to_archive = 'to_archive',
   archived = 'archived'
 }
@@ -10,6 +11,7 @@ export enum IntentStatus_Fr {
   to_deploy = 'À déployer',
   active = 'Actif',
   active_modified = 'Actif modifié',
+  in_training = 'Mise à jour en cours',
   to_archive = 'Actif à archiver',
   archived = 'Archivé'
 }
