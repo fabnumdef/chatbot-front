9a1c1b916e4f38057dd877290fb43a0e80e37b6b
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-12-16 19:59:19 +0100

    Feat: add check if id exists :sparkles:

--- src/app/core/services/intent.service.ts
+++ src/app/core/services/intent.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Intent } from '@model/intent.model';
 import { ApiPaginationService } from '@core/services/api-pagination.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class IntentService extends ApiPaginationService<Intent> {
               private _r: Router) {
     super(_http, '/intent', _r);
     this.loadAll().subscribe();
+  }
+
+  checkId(id: string): Observable<boolean> {
+    return this._http.get<any>(`${this._url}/check/${id}`);
   }
 }
--- src/app/modules/intent/create-edit-intent/intent-form/question-form/question-form.component.ts
+++ src/app/modules/intent/create-edit-intent/intent-form/question-form/question-form.component.ts
@@ -1,21 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map, startWith, takeUntil } from 'rxjs/operators';
 import { RefDataService } from '@core/services/ref-data.service';
+import { IntentService } from '@core/services/intent.service';
+import { DestroyObservable } from '@core/utils/destroy-observable';
 
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
   styleUrls: ['./question-form.component.scss']
 })
-export class QuestionFormComponent implements OnInit {
+export class QuestionFormComponent extends DestroyObservable implements OnInit {
 
   @Input() questionForm: FormGroup;
   categories$: BehaviorSubject<string[]>;
   filteredCategories$: Observable<string[]>;
 
-  constructor(private _refDataService: RefDataService) {
+  constructor(private _refDataService: RefDataService,
+              private _intentService: IntentService) {
+    super();
   }
 
   get controls() {
@@ -31,10 +35,36 @@ export class QuestionFormComponent implements OnInit {
           this.categories$.value.filter(option => option.toLowerCase().indexOf(category.toLowerCase()) === 0) :
           this.categories$.value.slice())
       );
+
+    this._checkIfIdExist();
   }
 
   isRequired(controls: AbstractControl) {
     // @ts-ignore
     return !!controls.validator('')?.hasOwnProperty('required');
+  }
+
+  private _checkIfIdExist() {
+    this.questionForm.get('id').valueChanges.pipe(
+      takeUntil(this.destroy$),
+      debounceTime(300),
+      distinctUntilChanged())
+      .subscribe(async (id) => {
+        if (id === this.questionForm.get('initialId').value || !id) {
+          if (this.questionForm.get('id').hasError('exists')) {
+            this.questionForm.get('id').setErrors({'exists': null});
+            this.questionForm.updateValueAndValidity();
+          }
+          return;
+        }
+        await this._intentService.checkId(id).subscribe(exists => {
+          if (exists) {
+            this.questionForm.get('id').setErrors({'exists': true});
+          } else if (this.questionForm.get('id').hasError('exists')) {
+            delete this.questionForm.get('id').errors['exists'];
+          }
+          this.questionForm.updateValueAndValidity();
+        });
+      });
   }
 }
