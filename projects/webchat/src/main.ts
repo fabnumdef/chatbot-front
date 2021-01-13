936a4440e2e2c3b18bd3be3fff560fc58d22f305
Vincent Lainé <vincent.laine.utc@gmail.com>
2021-01-13 16:10:46 +0100

    Feat: add message for old browsers :sparkles:

--- projects/webchat/src/main.ts
+++ projects/webchat/src/main.ts
@@ -8,5 +8,38 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const browser = (function(agent) {
+  switch (true) {
+    case agent.indexOf('edge') > -1:
+      return 'edge';
+    // @ts-ignore
+    case agent.indexOf('opr') > -1 && !!window.opr:
+      return 'opera';
+    // @ts-ignore
+    case agent.indexOf('chrome') > -1 && !!window.chrome:
+      return 'chrome';
+    case agent.indexOf('trident') > -1:
+      return 'ie';
+    case agent.indexOf('firefox') > -1:
+      return 'firefox';
+    case agent.indexOf('safari') > -1:
+      return 'safari';
+    default:
+      return 'other';
+  }
+})(window.navigator.userAgent.toLowerCase());
+console.log(browser);
+
+if (['ie', 'safari'].includes(browser)) {
+  document.getElementById('old-browsers').style.display = 'flex';
+} else {
+  platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+
+    // Otherwise, log the boot error
+  }).catch(err => console.error(err));
+}
